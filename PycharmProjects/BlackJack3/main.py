4d71f6bcb281e4af313829234f7feae949074e46
Alykar90 <alykar90@gmail.com>
2016-11-16 13:17:13 +0700

    logic

--- PycharmProjects/BlackJack3/main.py
+++ PycharmProjects/BlackJack3/main.py
@@ -3,6 +3,8 @@ import random
 help = '"_stop" - end game\n"_hitme" - more cards\n' \
        '"_enough" - no more cards\n"_hand" - all your cards\n' \
        '"_help" - view commands'
+
+'''
 class Card:
     def __init__(self,  name='', suit='', weight=0):
         self.name = name
@@ -17,7 +19,7 @@ class Card:
 
     def __eq__(self, other):
         return self.weight == other.weight and other.weight == self.weight
-
+'''
 
 class Game:
     def fitScore(self, n, score):
@@ -48,6 +50,34 @@ class Game:
         hand = self.filHand(card, hand)
         return score, hand
 
+    def checkScore(self, score, hand, sScore, sHand):
+        if score == 21:
+            print("BLACKJACK, YOU WON!\ndo you wish to try again?")
+
+        elif score > 21:
+            print('Your score over 21, you lost, prepare your cash!\ndo you wish to try again?')
+
+        elif score < 21:
+            print('now your score is:', score)
+            self.nextIteration(score, hand, sScore, sHand)
+
+    def gameWithoutPlayer(self, score, hand, sScore, sHand):
+        if score < sScore or sScore == 21:
+            print('your hand:', hand, '\nand dealers hand:', sHand,
+                  'dealer score is:', sScore, '\nand your score is:', score, '\nso...\nYou lost...',
+                  '\ndo you wish to try again?')
+            return
+
+        elif sScore > 21:
+            print('your hand:', hand, '\nand dealers hand:', sHand,
+                  'dealer score is:', sScore, '\nand your score is:', score, '\nso...\nYOU WON!',
+                  '\ndo you wish to try again?')
+            return
+
+        else:
+            sScore, sHand = self.newCard(sScore, sHand, 1)
+            self.gameWithoutPlayer(score, hand, sScore, sHand)
+
     def nextIteration(self, score, hand, sScore, sHand):
         phrase = input("what you gonna do? \n> ")
         if phrase == '_help':
@@ -56,20 +86,21 @@ class Game:
 
         elif phrase == '_hitme':
             score, hand = self.newCard(score, hand, 0)
-            sScore, sHand = self.newCard(sScore, sHand, 1)
-            print('now your score is:', score)
-            self.nextIteration(score, hand, sScore, sHand)
+            self.checkScore(score, hand, sScore, sHand)
 
         elif phrase == '_enough':
-            print('your hand:', hand,'and dealers hand:', sHand, 'so...')
-            if score > sScore:
-                print('YOU WON!')
-            else:
-                print('You lost...')
-            print("do you wish to try again?")
+            self.gameWithoutPlayer(score, hand, sScore, sHand)
 
         elif phrase == '_hand':
             print(hand)
+            self.nextIteration(score, hand, sScore, sHand)
+
+        elif phrase == '_stop':
+            print('I will accept your surrender!')
+            return
+
+        else:
+            print('wrong command, try again')
             self.nextIteration(score, hand, sScore, sHand)
 
     def newGame(self):
@@ -78,23 +109,25 @@ class Game:
         sScore = 0
         sHand = ''
         print('new game started')
-        score, hand = self.newCard(score, hand, 0)
-        score, hand = self.newCard(score, hand, 0)
-        print('now your score is:', score)
 
         sScore, sHand = self.newCard(sScore, sHand, 1)
         print('one of dealers card is ', sHand)
         sScore, sHand = self.newCard(sScore, sHand, 1)
+        score, hand = self.newCard(score, hand, 0)
+        score, hand = self.newCard(score, hand, 0)
+        print('now your score is:', score)
 
         self.nextIteration(score, hand, sScore, sHand)
         return
 
-
+print(help)
 while True:
-    print(help, '\nprint "_start" to start new game, or "_endgame" to leave.')
+    print('\nprint "_start" to start new game, or "_endgame" to leave.')
     phrase = input("> ")
     if phrase == '_start':
         Game().newGame()
     elif phrase == '_endgame':
         print('game ended...')
         break
+    else:
+        print('wrong command, try again')
